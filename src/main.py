aa9d57d6b32d0a309b4c47ea428ea05af6e45db2
Jayadeep Bejoy <miniminter2018p@gmail.com>
2025-03-06 23:46:16 +0530

    Fixed the daemon, prettified it, and answer generation is also fixed

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 def main(input_file_name = "testcase.txt", output_file_name = "output.txt"):
     input_file = open(input_file_name, "r")
-    first_line = input_file.readline().strip()
     output_file = open(output_file_name, "w")
+
+    first_line = input_file.readline().strip()
     first_line = first_line.split(";")
+
     output_file.write(f"{first_line[0]}={first_line[1]}/{1}/{1}\n")
+
     output_file.close()
     input_file.close()
 
